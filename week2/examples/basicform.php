5d2363721051933832a470893a88a24baabb6121
Nhat <ducnhatmmo98@gmail.com>
2020-03-15 23:09:36 +0700

    week2 exercises

--- week2/examples/basicform.php
+++ week2/examples/basicform.php
@@ -7,10 +7,10 @@
         <form action="http://fit.hut.edu.vn/~trangntt/courses/wp" method="POST">
             Click submit to start our initial PHP program.
             <br/>
-            <input type="text" name="name" value="">
-            <br>
-            <input type="submit" name="" value="Click to submit">
-            <input type="reset" name="" value="Erase and Restart">
+            <input type="text" name="name" value=""/>
+            <br/>
+            <input type="submit" name="" value="Click to submit"/>
+            <input type="reset" name="" value="Erase and Restart"/>
         </form>
     </body>
 </html>
