924a5c806905894602e1e8dedb27c7213f11bd2a
Nhat <ducnhatmmo98@gmail.com>
2020-03-17 13:32:59 +0700

    lab03 examples

---
+++ Lab02/examples/registrationform.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Registration Form</title>
+    </head>
+    <body>
+        <form action="http://fit.hut.edu.vn/~trangntt/courses/wp" method="POST">
+            First Name
+            <br/>
+            <input type="text" name="name" value=""/>
+            <br/>
+            Last Name
+            <br/>
+            <input type="text" name="name" value=""/>
+            <br/>
+            Email
+            <br/>
+            <input type="text" name="name" value=""/>
+            <br/>
+            Password
+            <br/>
+            <input type="text" name="name" value=""/>
+            <br/>
+            <input type="submit" name="" value="Click to submit"/>
+            <input type="reset" name="" value="Erase and Restart"/>
+        </form>
+    </body>
+</html>
