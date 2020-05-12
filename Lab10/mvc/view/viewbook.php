cb4f43050b1b7ea755b59a4062f322a3a1274617
Nhat <ducnhatmmo98@gmail.com>
2020-05-12 10:17:37 +0700

    add exercise1 lab10

---
+++ Lab10/mvc/view/viewbook.php
@@ -0,0 +1,16 @@
+<html>  
+<head></head>  
+
+<body>  
+
+    <?php   
+
+        echo 'Title:' . $book->title . '<br/>';  
+        echo 'Author:' . $book->author . '<br/>';  
+        echo 'Description:' . $book->description . '<br/>';  
+
+    ?>  
+
+</body>  
+</html>
+
